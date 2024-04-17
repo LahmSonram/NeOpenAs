154bed36dccb1968d7250b826fdd3f08e7dad987
LahmSonram <39515755+LahmSonram@users.noreply.github.com>
2024-04-17 21:55:17 +0700

    Add All File

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import chalk from 'chalk'
+import debug from 'debug'
+import morgan from 'morgan'
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get("/", (req,res) => {
+    res.send("LahmEiEi")
+})
+
+app.listen(port, () => {
+    debug("Listening on port", chalk.green(port))
+})
